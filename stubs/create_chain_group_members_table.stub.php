b7ad6b1fb39cee4e45b60176196ad817a2e9a779
Brian Rice <bwrice83@gmail.com>
2019-12-13 20:51:05 -0500

    fix migration class and file name

--- stubs/create_chain_group_members_table.stub.php
+++ stubs/create_chain_group_members_table.stub.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-final class CreateStoredEventsTable extends Migration
+final class CreateChainGroupMembersTable extends Migration
 {
     public function up()
     {
