63b2bd818d3ad1e689aab906f934785ba36b1555
Brian Rice <bwrice83@gmail.com>
2019-12-13 21:06:20 -0500

    create abstract TestCase

---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bwrice\LaravelJobChainGroups\Tests;
+
+use Illuminate\Support\Facades\Schema;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpDatabase();
+    }
+
+    protected function setUpDatabase()
+    {
+        Schema::dropIfExists('stored_events');
+        include_once __DIR__.'/../stubs/create_chain_group_members_table.stub.php';
+        (new \CreateChainGroupMembersTable())->up();
+    }
+}
