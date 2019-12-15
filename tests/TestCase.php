e493edeeb7a012ee7d0813a4d6b983ab518a2e08
Brian Rice <bwrice83@gmail.com>
2019-12-15 18:36:42 -0500

    add test chema to test setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Bwrice\LaravelJobChainGroups\Tests;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -15,6 +16,34 @@ abstract class TestCase extends Orchestra
 
     protected function setUpDatabase()
     {
+        /*
+         * Test Schema
+         */
+        Schema::dropIfExists('orders');
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->dateTime('preprocessed_at')->nullable();
+            $table->dateTime('shipped_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::dropIfExists('order_items');
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->integer('order_id')->unsigned();
+            $table->dateTime('processed_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+
+        /*
+         * Package Schema
+         */
         Schema::dropIfExists('chain_group_members');
         include_once __DIR__.'/../stubs/create_chain_group_members_table.stub.php';
         (new \CreateChainGroupMembersTable())->up();
