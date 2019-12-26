86871587779bb7ab5d49dc9b4d710b3ddf3cbe30
Brian Rice <bwrice83@gmail.com>
2019-12-26 12:13:14 -0500

    create PendingGroupDispatch in dispatchAsync

--- src/Traits/AsyncChainable.php
+++ src/Traits/AsyncChainable.php
@@ -4,6 +4,7 @@
 namespace Bwrice\LaravelJobChainGroups\Traits;
 
 
+use Bwrice\LaravelJobChainGroups\Bus\PendingGroupDispatch;
 use Bwrice\LaravelJobChainGroups\Jobs\AsyncChainedJob;
 use Bwrice\LaravelJobChainGroups\Models\ChainGroupMember;
 
@@ -11,9 +12,6 @@ trait AsyncChainable
 {
     public static function dispatchAsync(string $groupUuid, ...$jobArgs)
     {
-        $chainGroupMember = ChainGroupMember::query()->create([
-            'group_uuid' => $groupUuid
-        ]);
-        return AsyncChainedJob::dispatch($chainGroupMember->id, $groupUuid, new static(...$jobArgs));
+        return new PendingGroupDispatch($groupUuid, new static(...$jobArgs));
     }
 }
