219ceb50390018b369679c7c87db86d96fc07bca
Brian Rice <bwrice83@gmail.com>
2019-12-26 16:39:31 -0500

    create test job ShipOrder

---
+++ tests/TestClasses/Jobs/ShipOrder.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Bwrice\LaravelJobChainGroups\Tests\TestClasses\Jobs;
+
+
+use Bwrice\LaravelJobChainGroups\Tests\TestClasses\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Date;
+
+class ShipOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var Order
+     */
+    protected $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    public function handle()
+    {
+        $this->order->shipped_at = Date::now();
+        $this->order->save();
+    }
+}
