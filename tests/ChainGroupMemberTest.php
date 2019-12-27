fabef7e6fbba332acb9ec45910ababd3042d3278
Brian Rice <bwrice83@gmail.com>
2019-12-27 10:35:54 -0500

    update test for new schema

--- tests/ChainGroupMemberTest.php
+++ tests/ChainGroupMemberTest.php
@@ -4,7 +4,7 @@
 namespace Bwrice\LaravelJobChainGroups\Tests;
 
 
-use Bwrice\LaravelJobChainGroups\models\ChainGroupMember;
+use Bwrice\LaravelJobChainGroups\Models\ChainGroupMember;
 use Illuminate\Support\Str;
 
 class ChainGroupMemberTest extends TestCase
@@ -15,11 +15,12 @@ class ChainGroupMemberTest extends TestCase
     public function it_can_access_the_database()
     {
         $chainGroupMember = new ChainGroupMember();
+        $chainGroupMember->uuid = $uuid = (string) Str::uuid();
         $chainGroupMember->group_uuid = $groupUuid = (string) Str::uuid();
         $chainGroupMember->save();
 
         /** @var ChainGroupMember $retrievedMember */
-        $retrievedMember = ChainGroupMember::query()->find($chainGroupMember->id);
+        $retrievedMember = ChainGroupMember::query()->find($uuid);
         $this->assertEquals($groupUuid, $retrievedMember->group_uuid);
     }
 }
