0adb7346e54025d1b68e3d6040b055d8cfeaa771
Brian Rice <bwrice83@gmail.com>
2019-12-30 23:40:46 -0500

    add order-items relation to Order

--- tests/TestClasses/Models/Order.php
+++ tests/TestClasses/Models/Order.php
@@ -5,6 +5,7 @@ namespace Bwrice\LaravelJobChainGroups\Tests\TestClasses\Models;
 
 
 use Carbon\CarbonInterface;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -14,8 +15,13 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $id
  * @property CarbonInterface $preprocessed_at
  * @property CarbonInterface $shipped_at
+ *
+ * @property Collection $orderItems
  */
 class Order extends Model
 {
-
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
