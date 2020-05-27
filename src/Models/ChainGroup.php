17b54a2fafb163dc34ee4bd00b370e0da1013093
Brian Rice <bwrice83@gmail.com>
2020-05-27 14:40:25 -0400

    fix properties on ChainGroup model

--- src/Models/ChainGroup.php
+++ src/Models/ChainGroup.php
@@ -5,7 +5,6 @@ namespace Bwrice\LaravelJobChainGroups\Models;
 
 
 use Carbon\CarbonInterface;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,8 +12,7 @@ use Illuminate\Database\Eloquent\Model;
  * Class ChainGroup
  * @package Bwrice\LaravelJobChainGroups\models
  *
- * @property string $uuid
- * @property string $group_uuid
+ * @property int $id
  * @property CarbonInterface $processed_at
  *
  * @property Collection $chainGroupMembers
