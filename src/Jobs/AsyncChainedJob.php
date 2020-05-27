3ed167f381d414b92fc19ba62840676c4f66b459
Brian Rice <bwrice83@gmail.com>
2020-05-27 14:38:57 -0400

    use chain-group processed at to handle dispatching of next chained job

--- src/Jobs/AsyncChainedJob.php
+++ src/Jobs/AsyncChainedJob.php
@@ -3,6 +3,7 @@
 
 namespace Bwrice\LaravelJobChainGroups\Jobs;
 
+use Bwrice\LaravelJobChainGroups\Models\ChainGroup;
 use Bwrice\LaravelJobChainGroups\Models\ChainGroupMember;
 use Illuminate\Bus\Queueable;
 use Illuminate\Container\Container;
@@ -11,35 +12,40 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 
 class AsyncChainedJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, SerializesModels;
-    
+
     use Queueable {
         dispatchNextJobInChain as illuminateDispatchNextJobInChain;
     }
 
-    /** @var string */
-    protected $groupMemberUuid;
-
-    /** @var string */
-    protected $groupUuid;
-
-    /** @var mixed */
+    /**
+     * @var int
+     */
+    protected $chainGroupMemberID;
+    /**
+     * @var int
+     */
+    protected $chainGroupID;
+    /**
+     * @var mixed
+     */
     protected $decoratedJob;
 
-    public function __construct(string $groupMemberUuid, string $groupUuid, $decoratedJob)
+    public function __construct(int $chainGroupMemberID, int $chainGroupID, $decoratedJob)
     {
-        $this->groupMemberUuid = $groupMemberUuid;
-        $this->groupUuid = $groupUuid;
+        $this->chainGroupMemberID = $chainGroupMemberID;
+        $this->chainGroupID = $chainGroupID;
         $this->decoratedJob = $decoratedJob;
     }
 
     public function handle(Container $container)
     {
         /** @var ChainGroupMember $chainGroupMember */
-        $chainGroupMember = ChainGroupMember::query()->findOrFail($this->groupMemberUuid);
+        $chainGroupMember = ChainGroupMember::query()->findOrFail($this->chainGroupMemberID);
         $container->call([$this->decoratedJob, 'handle']);
 
         $chainGroupMember->processed_at = Date::now();
@@ -48,25 +54,33 @@ class AsyncChainedJob implements ShouldQueue
 
     public function dispatchNextJobInChain()
     {
-        if (ChainGroupMember::unprocessedForGroup($this->groupUuid)->count() === 0) {
-            $this->illuminateDispatchNextJobInChain();
+        /** @var ChainGroup $chainGroup */
+        $chainGroup = ChainGroup::query()->findOrFail($this->chainGroupID);
+        $unprocessedMembersCount = $chainGroup->chainGroupMembers()
+            ->where('processed_at', '=', null)
+            ->count();
+
+        /*
+         * If there are no more un-processed chain group members, this is the last, or, concurrently, one of the lasts
+         * async-chain-jobs for that group to be processed
+         */
+        if ($unprocessedMembersCount === 0) {
+
+            /*
+             * To prevent any race conditions, we'll create a transaction to check the processed_at state of the group,
+             *  and if it's unprocessed, dispatch the next job in the chain, and update the processed_at field
+             */
+            DB::transaction(function() use ($chainGroup) {
+
+                $processedAt = $chainGroup->fresh()->processed_at;
+                if (is_null($processedAt)) {
+
+                    $chainGroup->processed_at = now();
+                    $chainGroup->save();
+                    $this->illuminateDispatchNextJobInChain();
+                }
+            });
         }
-    }
-
-    /**
-     * @return string
-     */
-    public function getGroupMemberUuid(): string
-    {
-        return $this->groupMemberUuid;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGroupUuid(): string
-    {
-        return $this->groupUuid;
     }
 
     /**
@@ -81,5 +95,13 @@ class AsyncChainedJob implements ShouldQueue
     public function __call($method, $arguments)
     {
         return $this->decoratedJob->$method(...$arguments);
+    }
+
+    /**
+     * @return int
+     */
+    public function getChainGroupMemberID(): int
+    {
+        return $this->chainGroupMemberID;
     }
 }
